bebe8c066307567fc1eddc13793d056c10f31630
Vladislav Korniushenko <vkornushenko@gmail.com>
2025-11-15 23:39:45 +0200

    Interacting with DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement; // el that we want to put
   hostElement: HTMLDivElement; // el where we want to put el
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -16,7 +19,32 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+
+    // interacting with form element to style it by ID
+    this.element.id = 'user-input';
+
+    // getting access to form inputs
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+    this.configure();
     this.attache();
+  }
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  // set up an event listener
+  private configure() {
+    // calling bind() to preconfigure how submitHandler fn is going to execute
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   // method to insert form (element) in a div (hostElement)
