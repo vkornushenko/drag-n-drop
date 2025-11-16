d210ad40cb537bfb29add17f985f1ccc3afc5933
Vladislav Korniushenko <vkornushenko@gmail.com>
2025-11-16 21:31:34 +0200

    Rendering Project Lists

--- src/app.ts
+++ src/app.ts
@@ -56,6 +56,40 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+// ProjectList class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent(){
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private attach(){
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
 // ProjectInput class
 class ProjectInput {
   templateElement: HTMLTemplateElement; // el that we want to put
@@ -163,3 +197,6 @@ class ProjectInput {
 
 // instantiating a class to render a form
 const prjInput = new ProjectInput();
+// instantiating lists
+const active = new ProjectList('active');
+const finished = new ProjectList('finished');
